1725759de8abb9d5ac7cd361fc71c9e9a0969d29
yogita Avhad <yogita Avhad@LAPTOP-BB1QCNJ6>
2022-02-04 17:26:18 +0530

    Days.java

---
+++ JavaFullStack/src/Day.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Day
+
+{
+	public static void main(String[] args)
+	
+	{
+		Scanner s = new Scanner(System.in);
+	    System.out.println("Enter day between 0 to 6 Day=");
+	    int day = s.nextInt();
+		if(day ==0)
+		{
+			System.out.println("|n Sunday");
+		}
+		else if(day == 1)
+		{
+			System.out.println("|n Monday");
+		}
+		else if(day == 2)
+		{
+			System.out.println("|n Tuesday");
+		}
+		else if(day == 3)
+		{
+			System.out.println("|n Wednesday");
+		}
+		else if(day == 4)
+		{
+			System.out.println("|n Thursday");
+		}
+		else if(day == 5)
+		{
+			System.out.println("|n Friday");
+		}
+		else if(day == 6)
+		{
+			System.out.println("|n Saturday");
+		}
+		else if(day == 7)
+		{
+			System.out.println("|n Sunday");
+		}
+	}
+}
