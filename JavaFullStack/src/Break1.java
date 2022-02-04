babf6659729b2d7afdafc16c3235a28daee9f647
yogita Avhad <yogita Avhad@LAPTOP-BB1QCNJ6>
2022-02-04 18:17:13 +0530

    break1

---
+++ JavaFullStack/src/Break1.java
@@ -0,0 +1,19 @@
+
+public class Break1 
+{
+	public static void main(String[] args)
+	{
+		int i =1;
+		while(i<=20)
+		{
+			System.out.println("\n" + i);
+			i++;
+			if(i==10)
+			{
+				break;
+				
+			}
+		}
+	}
+
+}
