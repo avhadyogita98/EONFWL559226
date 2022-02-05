8dace47ab19a86a037b95a1bda1b79658ae9aec2
yogita Avhad <yogita Avhad@LAPTOP-BB1QCNJ6>
2022-02-05 12:41:39 +0530

    continue.commit

---
+++ JavaFullStack/src/Continue1.java
@@ -0,0 +1,19 @@
+
+public class Continue1
+{
+
+	public static void main(String[] args)
+	{
+		for(int i=1; i<20; i++)
+		{
+			if(i%2 ==0)
+			{
+				continue;
+			}
+			System.out.println("\n"+ i);
+			
+		}
+
+	}
+
+}
