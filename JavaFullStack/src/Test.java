82a04c5b5738299edb24dc65155550e019f02f92
yogita Avhad <yogita Avhad@LAPTOP-BB1QCNJ6>
2022-02-05 12:49:36 +0530

    Test is commit

---
+++ JavaFullStack/src/Test.java
@@ -0,0 +1,15 @@
+
+public class Test 
+{
+	public static void main(String[] args) 
+	{
+		boolean a = true;
+		boolean b = false;
+		
+		
+		System.out.println("a && b ="+(a&&b)); // false
+		System.out.println("a || b ="+(a||b)); // true
+		System.out.println("a && b ="+(a&&b)); // true
+	}
+
+}
