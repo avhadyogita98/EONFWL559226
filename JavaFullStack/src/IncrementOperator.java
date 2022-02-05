5c9ed825461f3d4ed4c93281400c9ae4d3b27815
yogita Avhad <yogita Avhad@LAPTOP-BB1QCNJ6>
2022-02-05 12:44:14 +0530

    Increment is 1st time Commit

---
+++ JavaFullStack/src/IncrementOperator.java
@@ -0,0 +1,17 @@
+
+public class IncrementOperator {
+
+	public static void main(String[] args) 
+	{
+		int a=15;
+		int b=5;
+		System.out.println("Value of a "+a);
+		System.out.println("Value of a "+b);
+		
+		System.out.println("Value of ++a "+(++a));
+		System.out.println("Value of a "+a);
+		System.out.println("Value of ++b "+(++b));
+		System.out.println("Value of b "+b);
+	}
+
+}
